5b6edbc9e8fabb866e842189f894111191c71edc
RajaomalalaSendra <rajaomalalasendrampanjato@gmail.com>
2019-01-24 16:30:32 +0300

    modification of bord and app

--- lib/app/board.rb
+++ lib/app/board.rb
@@ -18,7 +18,7 @@ class Board
     [@a1, @a2, @a3, @b1, @b2, @b3, @c1, @c2, @c3]
   
   end
-  def print_grid
+  def the_grid
     puts
     puts "   1   2   3"
     puts "A  #{@a1} | #{@a2} | #{@a3} " 
--- lib/views/application.rb
+++ lib/views/application.rb
@@ -8,21 +8,14 @@ class Application
   def perform
     # Creation partie
   my_game = Game.new
-  my_game.initialize_game
-
   # Creation du board
   my_board = Board.new
-
-  # Creation des cases du jeu (vide)
-  1.upto(9) { |id| my_board.add_case(BoardCase.new(id)) }
   # To get the two new players
   get_players = Show.new.show_player
   player1 = get_players[:player1]
   player2 = get_players[:player2]
-  # display the board 
-  my_board.display_board
-  # Boucle du jeu
-  game_turn(my_game, player1, player2, my_board)
+
+  puts my_board.the_grid
   end
 end
 
