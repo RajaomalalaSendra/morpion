8501a64bb019c267509b335a149e094055ab5b82
RajaomalalaSendra <rajaomalalasendrampanjato@gmail.com>
2019-01-24 23:51:38 +0300

    modifying the boardcase

--- lib/app/player.rb
+++ lib/app/player.rb
@@ -1,14 +1,30 @@
 # The class player for creating a new player
 require_relative "boardcase"
+require_relative "board"
 class Player
+	attr_accessor :a1, :a2, :a3, :b1, :b2, :b3, :c1, :c2, :c3
 	attr_reader :name, :value
-	boardcase = BoardCase.new
   # To initialize a new player
   def initialize(name, value)
     @name = name.capitalize
     @value = value
   end
+  def squares
+    [@a1, @a2, @a3, @b1, @b2, @b3, @c1, @c2, @c3]
+  end
+  def self.win_combos 
+    [[@a1, @a2, @a3],
+    [@a1, @b2, @c3],
+    [@a1, @b1, @c1],
+    [@b1, @b2, @b3],
+    [@c1, @c2, @c3],
+    [@c1, @b2, @a3],
+    [@a2, @b2, @c2],
+    [@a3, @b3, @c3]]
+  end
 	def self.user_one_turn
+	  boardcase = BoardCase.new
+	  board = Board.new
 	  user_choice_hash = {"a1" => @a1,
 	                      "a2" => @a2,
 	                      "a3" => @a3,
@@ -20,13 +36,15 @@ class Player
 	                      "c3" => @c3}
 	                    
 
-	  puts "Please choose a square:"
+	  puts "Please enter you choice #{@name}:"
 	  user_choice = gets.chomp.downcase
 	  user_choice_hash.each do |choice, square|
 	    if user_choice == choice 
-	      if boardcase.check_validity(square)
+	      if BoardCase.check_validity(square)
 	        square.sub!(" ", "X")
-	        print_grid
+	        puts square
+	        board.print_grid
+	        puts user_choice
 	      end
 	    elsif user_choice == "q" or user_choice == "quit"
 	      exit
@@ -36,6 +54,7 @@ class Player
 	end
 
 	def self.user_two_turn
+	  board = Board.new
 	  user_choice_hash = {"a1" => @a1,
 	                      "a2" => @a2,
 	                      "a3" => @a3,
@@ -47,13 +66,13 @@ class Player
 	                      "c3" => @c3}
 	                    
 
-	  puts "Please choose a square:"
+	  puts "Please enter you choice #{@name}"
 	  user_choice = gets.chomp.downcase
 	  user_choice_hash.each do |choice, square|
 	    if user_choice == choice 
-	      if boardcase.check_validity(square)
+	      if BroadCast.check_validity(square)
 	        square.sub!(" ", "O")
-	        print_grid
+	        board.print_grid
 	      end
 	    elsif user_choice == "q" or user_choice == "quit"
 	      exit
