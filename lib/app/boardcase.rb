f895dc92974af265673192ca9eb5eaae1ae13eba
RajaomalalaSendra <rajaomalalasendrampanjato@gmail.com>
2019-01-24 12:46:43 +0300

    adding the broadcase.rb

--- lib/app/boardcase.rb
+++ lib/app/boardcase.rb
@@ -1,12 +1,9 @@
+# Creation of the broadcast
 class BoardCase
-  #TO DO : la classe a 2 attr_accessor, sa valeur en string (X, O, ou vide), ainsi que son identifiant de case
   attr_accessor :int, :case
 
   def initialize(my_case)
-    #TO DO : doit régler sa valeur, ainsi que son numéro de case
     @int = 0
     @case = my_case
-  end
-  end
-  
+  end  
 end
--- lib/app/player.rb
+++ lib/app/player.rb
@@ -7,10 +7,4 @@ class Player
     @name = name.upcase
     @value = value
   end
-  def get
-  	print 'Nom du joueur 1 : '
-  	player1 = Player.new(gets.chomp.to_s, 1)
-  	print 'Nom du joueur 2 : '
-  	player2 = Player.new(gets.chomp.to_s, 2)
-  end
 end
