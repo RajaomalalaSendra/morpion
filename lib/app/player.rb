76ffe204db76f88e2760c53cdc5d2fcecd5583ce
RajaomalalaSendra <rajaomalalasendrampanjato@gmail.com>
2019-01-24 12:49:05 +0300

    modifying the player.rb

--- lib/app/player.rb
+++ lib/app/player.rb
@@ -2,9 +2,14 @@
 
 class Player
 	attr_reader :name, :value
-
+  # To initialize a new player
   def initialize(name, value)
     @name = name.upcase
     @value = value
+  end
+  # To call the turn of the player
+  def call_player
+    print "C'est à #{@name} de jouer : Choisissez une case à jouer entre 1 et 9 "
+    gets.chomp.to_i
   end
 end
